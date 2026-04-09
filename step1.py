115771d9eac9bd3f3b8db4d521dd793227bb33bf
Dgun89 <iszdg99@gmail.com>
2026-04-09 23:54:12 +0900

    step1: update get_cid function

--- step1.py
+++ step1.py
@@ -1,6 +1,6 @@
-import requests
+import requests # 1.라이브러리 불러오기
 
-def get_cid(name):
+def get_cid(name): # 2. 함수 정의
     url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{name}/cids/JSON"
     response = requests.get(url)
 
@@ -10,6 +10,6 @@ def get_cid(name):
     else:
         return None
 
-print(get_cid("Glucose"))
+print(get_cid("Glucose")) # 3. 함수 호출
 print(get_cid("Kynurenic acid"))
 print(get_cid("asdf1234"))
