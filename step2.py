a092830d628d7f8ef63dbea1360694a55bdd1478
Dgun89 <iszdg99@gmail.com>
2026-04-09 18:15:11 +0900

    step2: connect Excel B column to get_cid

--- step2.py
+++ step2.py
@@ -1,4 +1,13 @@
 import openpyxl
+import requests
+
+def get_cid(name):
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{name}/cids/JSON"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()["IdentifierList"]["CID"][0]
+    else:
+        return None
 
 wb = openpyxl.load_workbook("metabolites_completed.xlsx")
 ws = wb.active
@@ -8,4 +17,6 @@ ws = wb.active
 #print(ws.max_column)
 
 for row in ws.iter_rows(min_row=2, max_row=5, values_only=True):
-    print(row[1])
+    name = row[1]
+    cid = get_cid(name)
+    print(name, "→", cid)
